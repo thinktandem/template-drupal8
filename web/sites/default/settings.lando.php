743f2df482a5d2c465b514f4261e3f0ed971f467
John Ouellet <john@thinktandem.io>
2021-01-14 14:34:01 -0500

    Update to 8.9.12 + all deps

--- web/sites/default/settings.lando.php
+++ web/sites/default/settings.lando.php
@@ -8,17 +8,6 @@
 // @todo: eventually we want to remove this in favor of Lando directly
 // spoofing the needed PLATFORM_* envvars.
 if (isset($_SERVER['LANDO'])) {
-
-  // Set the database creds
-  $databases['default']['default'] = [
-    'database' => 'drupal8',
-    'username' => 'drupal8',
-    'password' => 'drupal8',
-    'host' => 'database',
-    'port' => '3306',
-    'driver' => 'mysql'
-  ];
-  
   // Only load dev settings on non admin pages.
   // This prevents very slow loading admin pages.
   // Especially when using some modules like layout builder.
@@ -30,9 +19,7 @@ if (isset($_SERVER['LANDO'])) {
     $config['system.performance']['css']['preprocess'] = FALSE;
     $config['system.performance']['js']['preprocess'] = FALSE;
   }
-
-  // And a bogus hashsalt for now
-  $settings['hash_salt'] = json_encode($databases);
+  
   // Fixes annoying perm issues on local.
   $settings['skip_permissions_hardening'] = TRUE;
 }
